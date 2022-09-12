410cf14f059cf55a42b52a4745883f820f82c542
Ale <alejandra.rodriguezub@uanl.edu.mx>
2022-09-12 15:19:21 -0500

    Cambié el nombre de la BD para crear la de Rimaldía

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://localhost:27017/PW2AGODIC2022", { useNewUrlParser: true })
+  .connect("mongodb://localhost:27017/Rimaldia", { useNewUrlParser: true })
   .then(() => console.log("Conectado a la base de datos MongoDB"))
   .catch(() => {
     console.log("No se pudo conectar con la base de datos");
