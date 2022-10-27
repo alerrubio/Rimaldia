cde889f287869883f1a5e1d6c0b3a1cf6be0484e
Ale <alejandra.rodriguezub@uanl.edu.mx>
2022-10-26 22:06:39 -0500

    pantalla de notificaciones

--- frontend/src/screens/test.jsx
+++ frontend/src/screens/test.jsx
@@ -1,24 +1,28 @@
 import "./css/Home.css";
 import { NewRhyme } from "../components/NewRhyme";
-import ForumCard from "../components/ForumCard";
-import UserNavigationBar from "../components/UserNavigationBar";
-import UserInfo from "../components/UserInfo";
-import Post from "../components/Post";
-import PP from"/img/pp-example.jpg";
+
+import NotificationCard from "../components/NotificationCard";
 var date = new Date();
 date = date.toLocaleDateString("es-MX",{ weekday:'long', day:'numeric', month:'long', year:'numeric'});
 function Home() {
 
   return (
     <>
-      <UserNavigationBar tabs={['Mis foros', 'Todos los foros']}/>
-        <div className="forum-cards-container d-flex flex-row flex-wrap col-12">
-          <ForumCard forum_name="Romance" icon="bi bi-tag-fill"></ForumCard>
-          <ForumCard forum_name="Poesía lírica" icon="bi bi-tag-fill"></ForumCard>
-          <ForumCard forum_name="Poesía épica" icon="bi bi-tag-fill"></ForumCard>
-          <ForumCard forum_name="Poesía dramática" icon="bi bi-tag-fill"></ForumCard>
-          <ForumCard forum_name="Poesía coral" icon="bi bi-tag-fill"></ForumCard>
-        </div>
+      <div className="col-12 d-flex flex-row flex-wrap justify-content-center">
+        
+        <NotificationCard title="Los usuarios más activos del mes" 
+                          text="¡Felicidades! Fulanito y panganito, fueron los usuarios con más post en el mes." 
+                          time={date}></NotificationCard>
+        <NotificationCard title="Los usuarios más activos del mes" 
+                          text="¡Felicidades! Fulanito y panganito, fueron los usuarios con más post en el mes." 
+                          time={date}></NotificationCard>
+        <NotificationCard title="Los usuarios más activos del mes" 
+                          text="¡Felicidades! Fulanito y panganito, fueron los usuarios con más post en el mes." 
+                          time={date}></NotificationCard>
+        <NotificationCard title="Los usuarios más activos del mes" 
+                          text="¡Felicidades! Fulanito y panganito, fueron los usuarios con más post en el mes." 
+                          time={date}></NotificationCard>
+      </div>
     </>
   )
 }
