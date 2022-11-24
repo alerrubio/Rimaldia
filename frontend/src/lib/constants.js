f260d9e0513bf7e6b965991c6e51d12953aa2bdb
Ale <alejandra.rodriguezub@uanl.edu.mx>
2022-11-24 00:46:44 -0600

    paginación de usuarios para admin y creación de admins

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -3,7 +3,12 @@ export const Constants = {
         ADMIN_ID: '637c33318a3d0bc0a9233344',
         POETA_ID: '637c33388a3d0bc0a9233346',
         SUPER_POETA_ID: '637c333e8a3d0bc0a9233348'
-    }
+    },
+    ROLES_TITLES: {
+        ADMIN: 'Administrador',
+        POETA: 'Poeta',
+        SUPER_POETA: 'Super Poeta'
+    },
 };
 
 export default Constants
--- frontend/src/screens/Users.jsx
+++ frontend/src/screens/Users.jsx
@@ -3,6 +3,7 @@ import UserCard from "../components/UserCard";
 import { getUsers } from "../services/usersService.js";
 import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Constants } from '../lib/constants.js';
 
 var datetime = new Date();
 datetime = datetime.toLocaleDateString("es-MX",{ weekday:'long', day:'numeric', month:'long', year:'numeric', hour:'numeric', minute:'numeric' });
@@ -12,11 +13,37 @@ date = date.toLocaleDateString("es-MX",{ weekday:'long', day:'numeric', month:'l
 function Users() {
   const { user, isAuthenticated, isLoading } = useAuth0();
   const [notAdmin, setnotAdmin] = useState(false);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
+  const [users, setUsers] = useState([]);
+  const [userCount, setUserCount] = useState(0);
 
-  const getAllUsers = async (page) => {
-    const users = await getUsers(page);
-    console.log(res);
+  const getAllUsers = async () => {
+    const response = await getUsers(page);
+    setUsers(response.data);
+    console.log(users);
+  }
+
+  let usersList = [];
+
+  {users.forEach((user, index)=>{
+    let role = "";
+    if (user.role == Constants.ROLES.POETA_ID){
+      role = Constants.ROLES_TITLES.POETA;
+    }
+    else if (user.role == Constants.ROLES.ADMIN_ID){
+      role = Constants.ROLES_TITLES.ADMIN;
+    }
+    else{
+      role = Constants.ROLES_TITLES.SUPER_POETA;
+    }
+      usersList.push(
+        <UserCard user_name={`${user.given_name} ${user.family_name}`} 
+          username={`${user.username}`} 
+          email={`${user.email}`}
+          user_id={`${user.user_id}`}
+          role={`${role}`}></UserCard>
+      );
+    })
   }
 
   useEffect(() => {
@@ -25,8 +52,13 @@ function Users() {
     if (adminUser == false){
       setnotAdmin(true);
     }
-    getAllUsers();
   }, [user]);
+
+  useEffect(() => {
+    getAllUsers();
+    setUserCount(users.length);
+    console.log(page);
+  }, [page]);
 
   if (isLoading){
     return (
@@ -51,26 +83,14 @@ function Users() {
     <>
       <div className="mt-3"></div>
       <div className="users-cards-container d-flex flex-row justify-content-center flex-wrap col-12">
-        <UserCard user_name="Sandra Noche" 
-                   username="sansan" 
-                   email="sansan@gmail.com"
-                   user_id="637c3fe7c3cfed678ac0e750"
-                   role="Poeta"></UserCard>
-        <UserCard user_name="Ricardo Lira" 
-                   username="rickylira"
-                   email="ricky@gmail.com"
-                   role="Poeta"></UserCard>
-        <UserCard user_name="Francisca Sueño" 
-                   username="panchitadream"
-                   email="panchita@gmail.com"
-                   role="Super poeta"></UserCard>
-        <UserCard user_name="Luisa Rimas"
-                   username="luiri"
-                   email="luiri@gmail.com"
-                   role="Poeta"></UserCard>
+        {usersList}
         <div className="col-12 d-flex flex-row justify-content-around mt-3 navigation">
-          <i class="bi bi-caret-left-fill nav-prev"></i>
-          <i class="bi bi-caret-right-fill nav-next"></i>
+          <i onClick={() => {
+            if (page > 1){
+              setPage(page - 1);
+            }
+            }} class="bi bi-caret-left-fill nav-prev"></i>
+          <i onClick={() => setPage(page + 1)} class="bi bi-caret-right-fill nav-next"></i>
         </div>
       </div>
       
