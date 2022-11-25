0f89f4b682bc35d5f5f0502b3c55efef72a3d9f5
Ale <alejandra.rodriguezub@uanl.edu.mx>
2022-11-25 01:08:28 -0600

    Comments WIP

--- frontend/src/components/CommentForm.jsx
+++ frontend/src/components/CommentForm.jsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 import Form from 'react-bootstrap/Form';
+import { useAuth0 } from "@auth0/auth0-react";
 
-const initialComment = {
-  user_id: ""
-}
-
-const CommentForm = (props) => {
+export const CommentForm = ({
+  post_id,
+  handleSubmit,
+  submitLabel,
+  hasCancelButton = false,
+  handleCancel,
+  initialText = "",
+  initialComment = {user_id: "", post_id: ""}
+}) => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-  const { handleSubmit, submitLabel, hasCancelButton, handleCancel, initialText } = props;
   const [text, setText] = useState(initialText);
   const [comment, setComment] = useState(initialComment);
   const isTextareaDisabled = text.length === 0;
@@ -21,26 +24,39 @@ const CommentForm = (props) => {
   const handleChange = (event) => {
     const { name, value } = event.target;
 
-    setUser({
-      ...user,
+    setComment({
+      ...comment,
+      user_id: user.sub.substring(6),
+      post_id: post_id,
       [name]: value,
     });
-    console.log(user);
+    console.log(comment);
   };
 
   useEffect(() => {
-    setComment({
-      ...comment,
-      [name]: value,
-    });
+    console.log(user);
   }, [user]);
+
+  if (isLoading){
+    return (
+    <>
+      <div className="loading d-flex justify-content-center align-items-center">
+        <img src={Logo} className="loadingLogo" alt="" />
+        <i class="bi bi-gear rotate"></i>
+      </div>
+    </>
+    );
+  }
   
   return (
     <Form onSubmit={onSubmit}>
       <textarea
         className="comment-form-textarea"
-        value={text}
-        onChange={handleChange()}
+        placeholder={text}
+        onChange={(e) => {
+          handleChange(e);
+          setText(e.target.value);
+        }}
       />
       <button className="comment-form-button" disabled={isTextareaDisabled}>
         {submitLabel}
